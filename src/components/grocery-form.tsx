02ae8157bebc0911a076d66b5b956a8e62a54881
Esha Santosh <esha.santosh04@gmail.com>
2025-06-15 13:52:42 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/components/grocery-form.tsx
+++ src/components/grocery-form.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import * as React from "react";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { useForm, type SubmitHandler } from "react-hook-form";
 import { z } from "zod";
