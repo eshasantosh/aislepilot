c834879fbe5d040edfbfbdfa63b5b2941972512f
Esha Santosh <esha.santosh04@gmail.com>
2025-06-16 04:51:33 +0000

    allow users to adjust the quantity of items in the shopping cart (- + bu

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -3,4 +3,5 @@ export const LOCAL_STORAGE_KEYS = {
   ITEMS_INPUT: 'aislePilot_itemsInput',
   CATEGORIZED_LIST: 'aislePilot_categorizedList',
   CHECKED_ITEMS: 'aislePilot_checkedItems',
+  ITEM_QUANTITIES: 'aislePilot_itemQuantities',
 };
